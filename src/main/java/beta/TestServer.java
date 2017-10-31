446e94fbd0942e6c7cbd63cd4062f534a7b4307f
aleksej <aleksey0398@icloud.com>
2017-10-31 17:38:58 +0500

    small changes

---
+++ src/main/java/beta/TestServer.java
@@ -0,0 +1,45 @@
+package beta;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class TestServer {
+    private static String url1 = "http://92.248.221.177:8765/v_gru=2260&v_date=30.10.2017&type=ochnoe";
+    private static String url2 = "http://92.248.221.177:8765/getCountConnection";
+    private static String url3 = "http://92.248.221.177:8765/getAllOchnoe";
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                try {
+                    new URL(url1).openStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                try {
+                    new URL(url2).openStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                try {
+                    new URL(url3).openStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+    }
+}
